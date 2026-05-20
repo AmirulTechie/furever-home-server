491277f66caab4549887aae9fdcca65765c8541e
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-05-20 21:52:52 +0600

    chore: connected mongodb

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require('dotenv');
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,14 +17,48 @@ app.get('/', (req, res) => {
   res.send('FurEver Home API running');
 });
 
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    
+    const db = client.db('fureverdb');
+    const petsCollection = db.collection('pets');
+
+    app.get('/pets', async(req,res)=>{
+      const {courseId} = req.params;
+      const result = await petsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/pets/:petId', async(req,res)=>{
+      const petId = req.params.petId;
+      const query = { _id: petId };
+      const result = await petsCollection.findOne(query);
+      res.send(result);
+    })
 
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
-      console.log('Server running on port ' + process.env.PORT);
-    });
-  })
-  .catch((err) => console.error(err));
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
